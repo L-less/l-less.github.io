74f0502025918429ef8fb2307795b9cc88061dbd
L-less <160574396+L-less@users.noreply.github.com>
2024-03-03 15:20:03 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,11 +2,11 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/vue/",
 
   locales: {
     "/": {
-      lang: "en-US",
+      lang: "zh-CN",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
     },
