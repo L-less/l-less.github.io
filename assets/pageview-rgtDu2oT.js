e497d9a9f31af1549b8c793bd08ddcdefa8c5a92
L-less <160574396+L-less@users.noreply.github.com>
2024-03-09 11:58:28 +0000

    Deploying to gh-pages from @ L-less/l-less.github.io@66f7cf7091625f0241dc74ec855a7d517b7f6d7b 🚀

---
+++ assets/pageview-rgtDu2oT.js
@@ -0,0 +1,7 @@
+import{_ as r}from"./app-BfbC3Tlb.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://waline-1-v2177860.deta.app"};const n=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-BfbC3Tlb.js").then(t=>t.L),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{n as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
