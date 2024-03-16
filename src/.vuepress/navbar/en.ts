a797325c93cd4ae4046503639bda5eaf6c83585f
l-less <11909740+L-less@user.noreply.gitee.com>
2024-03-16 08:44:56 +0000

    write-button
    Signed-off-by: l-less <11909740+L-less@user.noreply.gitee.com>

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -11,4 +11,9 @@ export const enNavbar = navbar([
       { text: "class-note-1", icon: "pen-to-square", link: "dragonfruit" },
     ],
   },
+  {
+    text: "Write",
+    icon: "fa-pencil-square",
+    prefix: "https://gitee.com/L-less/blog/new/main/src/posts",
+  }
 ]);
